473f4a5fd0608c553ff03547cea431b597602010
Mac OS <mydevteam@Macs-iMac.local>
2022-12-12 13:30:34 +0700

    add bulma

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
+<!-- <style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style> -->
 
 <template>
   <div>
@@ -12,6 +15,13 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+
+  <div class="columns is-multiline is-mobile is-vcentered is-desktop column-reverse">
+    <div class="column is-4-tablet is-full-touch ">
+      <div class="cek">cek</div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
@@ -20,9 +30,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
