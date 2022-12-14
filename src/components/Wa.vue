6bd51102b44c86c2606e678ad0791adbfd6ed928
Mac OS <mydevteam@Macs-iMac.local>
2022-12-14 16:52:48 +0700

    revision fb pixel

--- src/components/Wa.vue
+++ src/components/Wa.vue
@@ -15,28 +15,24 @@
 </template>
 
 <script>
-
-!function (f, b, e, v, n, t, s) {
-  if (f.fbq) return; n = f.fbq = function () {
-    n.callMethod ?
-      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-  };
-  if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
-  n.queue = []; t = b.createElement(e); t.async = !0;
-  t.src = v; s = b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t, s)
-}(window, document, 'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '844457336851471');
-fbq('track', 'PageView');
-fbq('track', 'Lead');
-
-
-
-
 export default {
   name: "app",
   mounted() {
+
+    !function (f, b, e, v, n, t, s) {
+      if (f.fbq) return; n = f.fbq = function () {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+      n.queue = []; t = b.createElement(e); t.async = !0;
+      t.src = v; s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '844457336851471');
+    fbq('track', 'PageView');
+    fbq('track', 'Lead');
 
     setTimeout(function () {
       window.location = "https://wa.me/6289699022720?text=Hai mom, saya mau order produk Moell";
