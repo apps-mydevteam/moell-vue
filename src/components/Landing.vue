62289e200f7095dba555c563078d91a5910b43d4
Mac OS <mydevteam@Macs-iMac.local>
2023-10-04 16:14:46 +0700

    updatenatural marketlink

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -6,118 +6,124 @@ defineProps({
 </script>
 
 <template>
-  <section class="hero is-large banner-full pb-4">
+  <section class="hero is-large banner-full-v2 pb-4">
     <img src="../assets/images/logo.png" alt="">
-    <p>express your love by protecting your baby's skin <br>100% natural ingredients perfect for sensitive skin</p>
+    <!-- <p>express your love by protecting your baby's skin <br>100% natural ingredients perfect for sensitive skin</p> -->
   </section>
-  <section class="section is-medium px-0 is-relative pt-0">
+  <section class="section is-medium px-5 pb-0 is-relative">
+    <div class="container">
+      <div class="columns is-multiline is-mobile is-vcentered is-centered px-0 is-justify-content-flex-start">
+        <div class="column is-3-mobile is-4-tablet pl-0">
+          <div class="divider"></div>
+        </div>
+        <div class="column is-6-mobile is-4-tablet has-text-centered">
+          <h2 class="font-poppins-head">Order Via</h2>
+        </div>
+        <div class="column is-3-mobile is-4-tablet pr-0">
+          <div class="divider"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section is-medium py-6 is-relative px-3">
     <div class="container">
       <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
-        <div class="column is-full-mobile is-12-tablet px-0 add-padding">
-          <div class="columns is-multiline is-mobile is-vcentered is-centered px-0 ">
-            <div class="column is-3-mobile is-3-tablet mr-4">
-              <a href="/out?url=https://shop.tiktok.com/view/product/1729574090104604743?region=ID&locale=id-ID"
-                target="_blank">
-                <div class="wrap-button grow-scale">
-                  <div class="wrap-img">
-                    <img src="../assets/images/icon-tiktok-1.png" alt="">
-                  </div>
-
-                  <div class="wrap-text">
-                    Order via Tiktok
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="column is-3-mobile is-3-tablet">
-              <a href="/wa" target="_blank">
-                <div class="wrap-button">
-                  <div class="wrap-img"><img src="../assets/images/icon-wa-1.png" alt=""></div>
-
-                  <div class="wrap-text">
-                    Order via Whatsapp
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="column is-3-mobile is-3-tablet">
-              <a href="/out?url=https://shope.ee/7KQ5ZhUvAG" target="_blank">
-                <div class="wrap-button">
-                  <div class="wrap-img"><img src="../assets/images/shopee.png" alt=""></div>
-
-                  <div class="wrap-text">
-                  Order via Shopee
-                </div>
-                </div>
-              </a>
-            </div>
-
-            <!-- <div class="column is-9-mobile is-12-tablet">
-                    <img src="../assets/images/bg-8.png" alt="" class="img-2">
-                  </div> -->
-            <div class="column is-9-mobile is-12-tablet">
-              <p class="add-text-natural">our natural organic product:</p>
-            </div>
-          </div>
-
-
+        <div class="column is-6-mobile is-3-tablet has-text-centered px-0">
+          <a href="/wa" target="_blank">
+            <img src="../assets/images/button_whatsapp.png" alt="">
+          </a>
         </div>
+        <div class="column is-6-mobile is-3-tablet px-0">
+          <a href="/out?url=https://shope.ee/9zS6s44aVE" target="_blank">
+            <img src="../assets/images/button_shopee.png" alt="">
+          </a>
+        </div>
+        <div class="column is-6-mobile is-3-tablet has-text-centered px-0">
+          <a href="/out?url=https://www.tokopedia.com/moellofficial?source=universe&st=product" target="_blank">
+            <img src="../assets/images/button_tokped.png" alt="">
+          </a>
+        </div>
+        <div class="column is-6-mobile is-3-tablet has-text-centered px-0">
+          <a href="/lazada" target="_blank">
+            <img src="../assets/images/button-lazada.png" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section is-medium px-5 py-0 is-relative">
+    <div class="container">
+      <div class="columns is-multiline is-mobile is-vcentered is-centered px-0 is-justify-content-flex-start">
+        <div class="column is-2-mobile is-3-tablet pl-0">
+          <div class="divider add-height"></div>
+        </div>
+        <div class="column is-8-mobile is-6-tablet has-text-centered">
+          <h2 class="font-poppins-head">Our Natural<br>Organic Product</h2>
+        </div>
+        <div class="column is-2-mobile is-3-tablet pr-0">
+          <div class="divider add-height"></div>
+        </div>
+      </div>
+      <div class="columns is-multiline is-mobile is-vcentered is-centered px-0 is-justify-content-flex-start">
+        <div class="column is-12-mobile is-12-tablet pl-0 has-text-centered">
+          <p class="subtitle">Perawatan natural terbaik untuk kulit si kecil</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section is-medium px-0 pb-5 is-relative pt-6">
+    <div class="container">
+      <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
+
         <div class="column is-full-mobile is-12-tablet p-0" v-for="item in products" :key="item.id">
           <img :src="item.imgProductUrl" alt="" class="imgProduct">
         </div>
-        <div class="column is-12-mobile is-12-tablet pt-0 is-relative">
-          <p class="text-intro">Everyone Loves Moell</p>
-          <!-- <p class="text-intro">What they say about Moell Natural Care for Babies</p> -->
-          <img src="../assets/images/bg-7.png" alt="" class="img-testi">
-        </div>
-        <div class="column is-12-mobile is-12-tablet">
-          <!-- <div class="divider"></div> -->
-          <div class="columns is-multiline is-mobile is-vcentered is-centered mt-5">
-            <div class="column is-3-mobile is-3-tablet mr-4">
-              <a href="/out?url=https://shop.tiktok.com/view/product/1729574090104604743?region=ID&locale=id-ID"
-                target="_blank">
-                <div class="wrap-button grow-scale">
-                  <div class="wrap-img">
-                    <img src="../assets/images/icon-tiktok-1.png" alt="">
-                  </div>
-
-                  <div class="wrap-text">
-                    Order via Tiktok
-                  </div>
-                </div>
-              </a>
+        <div class="column is-12-mobile is-12-tablet add-padding-top is-relative">
+          <div class="columns is-multiline is-mobile is-vcentered is-centered px-0 is-justify-content-flex-start">
+            <div class="column is-3-mobile is-3-tablet">
+              <div class="divider"></div>
+            </div>
+            <div class="column is-6-mobile is-6-tablet has-text-centered">
+              <h2 class="font-poppins-head">Everyone Loves Moell</h2>
             </div>
             <div class="column is-3-mobile is-3-tablet">
-              <a href="/wa" target="_blank">
-                <div class="wrap-button">
-                  <div class="wrap-img"><img src="../assets/images/icon-wa-1.png" alt=""></div>
-
-                  <div class="wrap-text">
-                    Order via Whatsapp
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="column is-3-mobile is-3-tablet">
-              <a href="/out?url=https://shope.ee/7KQ5ZhUvAG" target="_blank">
-                <div class="wrap-button">
-                  <div class="wrap-img"><img src="../assets/images/shopee.png" alt=""></div>
-
-                  <div class="wrap-text">
-                  Order via Shopee
-                </div>
-              </div>
-              </a>
+              <div class="divider"></div>
             </div>
           </div>
-          <!-- <div class="wrapper-icon">
-                  <a href="/out" class="link" target="_blank"><img src="../assets/images/icon-tiktok-2.png" alt=""></a>
-                  <a href="/wa" class="link" target="_blank"><img src="../assets/images/icon-wa-2.png" alt=""></a>
-                </div> -->
+        </div>
+        <div class="column is-12-mobile is-12-tablet pt-0 is-relative">
+          <img src="../assets/images/bg-10.png" alt="" class="img-testi">
         </div>
       </div>
+    </div>
+  </section>
 
-
+  <section class="section is-medium py-6 is-relative px-3">
+    <div class="container">
+      <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
+        <div class="column is-6-mobile is-3-tablet has-text-centered px-0">
+          <a href="/wa" target="_blank">
+            <img src="../assets/images/button_whatsapp.png" alt="">
+          </a>
+        </div>
+        <div class="column is-6-mobile is-3-tablet px-0">
+          <a href="/out?url=https://shope.ee/9zS6s44aVE" target="_blank">
+            <img src="../assets/images/button_shopee.png" alt="">
+          </a>
+        </div>
+        <div class="column is-6-mobile is-3-tablet has-text-centered px-0">
+          <a href="/out?url=https://www.tokopedia.com/moellofficial?source=universe&st=product" target="_blank">
+            <img src="../assets/images/button_tokped.png" alt="">
+          </a>
+        </div>
+        <div class="column is-6-mobile is-3-tablet has-text-centered px-0">
+          <a href="/lazada" target="_blank">
+            <img src="../assets/images/button-lazada.png" alt="">
+          </a>
+        </div>
+      </div>
     </div>
   </section>
 </template>
