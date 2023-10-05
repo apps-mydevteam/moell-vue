8c15922c7600f9c17f5d89da0443cd69475d2aff
Mac OS <mydevteam@Macs-iMac.local>
2023-10-05 16:42:14 +0700

    change size image

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -12,7 +12,7 @@ defineProps({
         <img src="../assets/images/logo.png" alt="">
         <!-- <p>express your love by protecting your baby's skin <br>100% natural ingredients perfect for sensitive skin</p> -->
     </section>
-    <section class="section is-medium px-5 pb-0 is-relative">
+    <section class="section is-medium pt-6 px-5 pb-0 is-relative">
         <div class="container">
             <div class="columns is-multiline is-mobile is-vcentered is-centered px-0 is-centered">
                 <div class="column is-3-mobile is-3-tablet pl-0">
@@ -33,22 +33,24 @@ defineProps({
             <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
                 <div class="column is-12-mobile is-8-tablet has-text-centered px-0">
                     <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
-                        <div class="column is-6-mobile is-6-tablet has-text-centered px-0">
+                        <div class="column is-4-mobile is-6-tablet has-text-centered px-0">
                             <a href="/wa" target="_blank">
                                 <img src="../assets/images/button_whatsapp.png" alt="">
                             </a>
                         </div>
-                        <div class="column is-6-mobile is-6-tablet px-0">
+                        <div class="column is-4-mobile is-6-tablet px-0">
                             <a href="/out?url=https://shope.ee/9KFILGsQWp" target="_blank">
                                 <img src="../assets/images/button_shopee.png" alt="">
                             </a>
                         </div>
-                        <div class="column is-6-mobile is-6-tablet has-text-centered px-0">
+                    </div>
+                    <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
+                        <div class="column is-4-mobile is-6-tablet has-text-centered px-0">
                             <a href="/out?url=https://www.tokopedia.link/qv1gD0tjBCb" target="_blank">
                                 <img src="../assets/images/button_tokped.png" alt="">
                             </a>
                         </div>
-                        <div class="column is-6-mobile is-6-tablet has-text-centered px-0">
+                        <div class="column is-4-mobile is-6-tablet has-text-centered px-0">
                             <a href="/lazada" target="_blank">
                                 <img src="../assets/images/button-lazada.png" alt="">
                             </a>
@@ -106,27 +108,29 @@ defineProps({
         </div>
     </section>
 
-    <section class="section is-medium py-6 is-relative px-3">
+    <section class="section is-medium pb-6 is-relative px-3 pt-0">
         <div class="container">
             <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
                 <div class="column is-12-mobile is-8-tablet has-text-centered px-0">
                     <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
-                        <div class="column is-6-mobile is-6-tablet has-text-centered px-0">
+                        <div class="column is-4-mobile is-6-tablet has-text-centered px-0">
                             <a href="/wa" target="_blank">
                                 <img src="../assets/images/button_whatsapp.png" alt="">
                             </a>
                         </div>
-                        <div class="column is-6-mobile is-6-tablet px-0">
+                        <div class="column is-4-mobile is-6-tablet px-0">
                             <a href="/out?url=https://shope.ee/9KFILGsQWp" target="_blank">
                                 <img src="../assets/images/button_shopee.png" alt="">
                             </a>
                         </div>
-                        <div class="column is-6-mobile is-6-tablet has-text-centered px-0">
+                    </div>
+                    <div class="columns is-multiline is-mobile is-vcentered is-centered px-0">
+                        <div class="column is-4-mobile is-6-tablet has-text-centered px-0">
                             <a href="/out?url=https://www.tokopedia.link/qv1gD0tjBCb" target="_blank">
                                 <img src="../assets/images/button_tokped.png" alt="">
                             </a>
                         </div>
-                        <div class="column is-6-mobile is-6-tablet has-text-centered px-0">
+                        <div class="column is-4-mobile is-6-tablet has-text-centered px-0">
                             <a href="/lazada" target="_blank">
                                 <img src="../assets/images/button-lazada.png" alt="">
                             </a>
